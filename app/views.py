94b0ec3ae7bb4e4a18fd3706b3007d3de5a5cc37
4books <joonwoo9192@gmail.com>
2022-10-01 20:39:28 +0900

    added app/index viex

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+from app.models import Post
+
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    qs = Post.objects.all()
+    # qs = [
+    #     {"id": 1, "title": "post #1"},
+    #     {"id": 2, "title": "post #2"},
+    # ]
+    return render(
+        request,
+        "app/index.html",
+        {
+            "post_list": qs,
+        },
+    )
